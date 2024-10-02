82bc3500facae2d5a1becaf620dea1da5f57b5f4
evanfahringer <evanfahr@ad.unc.edu>
2024-10-02 01:23:34 -0400

    Add files via upload

---
+++ Assignment 3/Assignment3.py
@@ -0,0 +1,8 @@
+terms = ['loops', 'strings', 'statements', 'functions']
+
+for term in terms:
+    if term == "statements":
+        print(term.title()) # you may use any string method you wish.
+    else:
+        print(term.upper()) # you man use any string method you wish.
+        
